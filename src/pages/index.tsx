727019fc1e2d83392c511231df65602635eb99a5
Lucas Freitas <l.freittas@outlook.com>
2024-04-24 20:06:31 -0300

    fetching products in stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,19 @@ import 'keen-slider/keen-slider.min.css'
 import camiseta1 from '../assets/camisa1.png';
 import camiseta2 from '../assets/camisa2.png';
 import camiseta3 from '../assets/camisa3.png';
+import { GetServerSideProps } from "next";
+import { stripe } from "@/libs/stripe";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     loop: true,
     slides: {
@@ -20,32 +31,41 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image  src={camiseta1} width={520} height={480} alt='' />
+      {products.map(product => {
+        return(
+          <Product key={product.id} className="keen-slider__slide">
+            <Image  src={product.imageUrl} width={520} height={480} alt='' />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image  src={camiseta2} width={520} height={480} alt='' />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image  src={camiseta3} width={520} height={480} alt='' />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+        </Product>
+        )
+      } )}
     </HomeContainer>
   )
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  });
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products
+    }
+  }
 };

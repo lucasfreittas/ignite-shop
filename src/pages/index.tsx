c1ae33632a38fb3de6103920fe32b9b57dbdaa52
Lucas Freitas <l.freittas@outlook.com>
2024-04-24 20:32:13 -0300

    nav via link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { HomeContainer, Product } from "@/styles/pages/home";
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
+import Link from "next/link";
 
 import { GetStaticProps } from "next";
 import { stripe } from "@/libs/stripe";
@@ -31,14 +32,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return(
-          <Product key={product.id} className="keen-slider__slide">
-            <Image  src={product.imageUrl} width={520} height={480} alt='' />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image  src={product.imageUrl} width={520} height={480} alt='' />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-        </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+        </Link>
         )
       } )}
     </HomeContainer>

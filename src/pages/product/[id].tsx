9d897e9e952461bcf8f067440fc53a3f1afd0a3f
Lucas Freitas <l.freittas@outlook.com>
2024-04-24 20:47:55 -0300

    products page css

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProcutContainer, ProductDetails } from "@/styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProcutContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta 1</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum repellat voluptates impedit ipsum dolore eius reprehenderit sapiente similique qui expedita. A enim repellendus accusamus sequi cum porro eum, at laboriosam.</p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProcutContainer>
+
   )
 }
